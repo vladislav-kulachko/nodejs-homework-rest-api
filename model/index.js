c6605258dd220f86962a00b10815d9645e3baad2
Vladislav Kulachko <vladcoool@gmail.com>
2021-11-15 00:45:53 +0200

    первое завершение многофакторной валидации

--- model/index.js
+++ model/index.js
@@ -1,20 +1,64 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require("fs/promises")
+const {pathToFileURL} = require("url")
+const path = require("path")
+const randId = require("crypto").randomUUID
 
-const listContacts = async () => {}
+const contactsPath = pathToFileURL("model/contacts.json")
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath)
+  return JSON.parse(contacts)
+}
 
-const removeContact = async (contactId) => {}
+const getContactById = async contactId => {
+  const list = await listContacts()
+  const contactById = list.find(({id}) => String(id) === contactId)
+  return contactById
+}
 
-const addContact = async (body) => {}
+const removeContact = async contactId => {
+  const list = await listContacts()
+  const find = list.find(({id}) => String(id) === contactId)
+  if (find) {
+    const filteredList = list.filter(({id}) => String(id) !== contactId)
+    await fs.writeFile(contactsPath, JSON.stringify(filteredList, null, 2))
+  }
+  return find
+}
 
-const updateContact = async (contactId, body) => {}
+const addContact = async body => {
+  const newContact = {id: randId(), ...body}
+  const list = await listContacts()
+  list.push(newContact)
+  await fs.writeFile(contactsPath, JSON.stringify(list, null, 2))
+  return newContact
+}
+
+const updateContact = async (contactId, body) => {
+  const {name, phone, email} = body
+  const contact = await getContactById(contactId)
+  if (contact) {
+    if (name) {
+      contact.name = name
+    }
+    if (phone) {
+      contact.phone = phone
+    }
+    if (email) {
+      contact.email = email
+    }
+    const list = await listContacts()
+    const filteredList = list.filter(({id}) => String(id) !== contactId)
+    const newList = [...filteredList, contact]
+    await fs.writeFile(contactsPath, JSON.stringify(newList, null, 2))
+  }
+  return contact
+}
 
 module.exports = {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-  updateContact,
+  updateContact
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,143 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
+const {NotFound, BadRequest} = require("http-errors")
+const {checkSchema, validationResult} = require("express-validator")
+const operations = require("../../model/index")
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const validationRules = checkSchema({
+  name: {
+    in: ["body"],
+    notEmpty: true,
+    errorMessage: "Пожалуйста введите имя!",
+    isLength: {
+      options: {max: 25},
+      errorMessage: "Превышен лимит. Максимальная длинна имени 25 символов."
+    },
+    matches: {
+      options: ["^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"],
+      errorMessage: "Имя может содержать только буквы, тире, пробелы, апостроф."
+    },
+    trim: true
+  },
+  phone: {
+    in: ["body"],
+    notEmpty: true,
+    errorMessage: "Пожалуйста введите номер!",
+    // isMobilePhone: true,
+    isLength: {
+      options: {min: 10, max: 13},
+      errorMessage:
+        "Длинна номера должна быть 10 символов в сокращенном формате и 13 в международном без учета скобок."
+    },
+    matches: {
+      options: [
+        "^([+]\\d{2})?\\s?[(]?\\d{3}[)]?\\s?\\d{3}[-]?\\d{2}[-]?\\d{2}$"
+      ],
+      errorMessage:
+        "Введите номер телефона в формате:  +01 (234) 567-89-99, +01 (234) 5678999, +012345678999, 0123456789"
+    },
+    custom: {
+      options: async value => {
+        const list = await operations.listContacts()
+        if (list.find(({phone}) => phone === value)) {
+          return await Promise.reject(
+            new Error(`Номер ${value} уже находится в спискe контактов`)
+          )
+        }
+      }
+    },
+    trim: true
+  },
+  email: {
+    in: ["body"],
+    isEmail: {
+      args: true,
+      errorMessage:
+        "Введите действительный электронный адрес в формате 'имя_пользователя@имя_домена' "
+    },
+    normalizeEmail: true,
+    custom: {
+      options: async value => {
+        const list = await operations.listContacts()
+        if (list.find(({email}) => email === value)) {
+          return await Promise.reject(
+            new Error(`Почта ${value} уже находится в спискe контактов`)
+          )
+        }
+      }
+    }
+  }
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+const validate = validations => {
+  return async (req, res, next) => {
+    await Promise.all(validations.map(validation => validation.run(req)))
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+      return next()
+    }
+    next(BadRequest(errors.array()))
+    // res.status(400).json({
+    //   errors: errors.array(),
+    // })
+  }
+}
+
+router.get("/", async (req, res, next) => {
+  try {
+    const contacts = await operations.listContacts()
+    res.status(200).json({status: "success", data: {contacts}})
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const {contactId} = req.params
+    const contactById = await operations.getContactById(contactId)
+    if (contactById) {
+      res.status(200).json({status: "success", data: contactById})
+    }
+    throw new NotFound(`Contact with id ${contactId} not found`)
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post("/", validate(validationRules), async (req, res, next) => {
+  try {
+    const addedContact = await operations.addContact(req.body)
+    res.status(201).json({status: "success", data: addedContact})
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const {contactId} = req.params
+    const deletedContact = await operations.removeContact(contactId)
+    if (deletedContact) {
+      res.status(200).json({status: "success", data: deletedContact})
+    }
+    throw new NotFound(`Delete fail. Contact with id ${contactId} not found`)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put("/:contactId", validate(validationRules), async (req, res, next) => {
+  try {
+    const {contactId} = req.params
+    const updContact = await operations.updateContact(contactId, req.body)
+    if (updContact) {
+      res.status(200).json({status: "success", data: updContact})
+    }
+    throw new NotFound(`Update fail. Contact with id ${contactId} not found`)
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
