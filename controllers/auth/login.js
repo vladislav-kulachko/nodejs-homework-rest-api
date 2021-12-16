1cbb0582e33f7d2f0e50cbfd40aa5a4409d42681
Vladislav Kulachko <vladcoool@gmail.com>
2021-12-16 05:44:24 +0200

    add login hours

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -18,7 +18,7 @@ const login = async (req, res) => {
   }
 
   const payload = {id: user._id}
-  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "1h"})
+  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "12h"})
   const userWithToken = await User.findByIdAndUpdate(
     user._id,
     {token},
